d1b695050825e1c9cce02ed347373483a81e13c4
CallMeDrewChristian <72636893+CallMeDrewChristian@users.noreply.github.com>
2026-01-27 16:24:10 -0500

    Added Random Force & Modified Gravity Scale

--- LostInSpace/Assets/Scripts/Scripts_Gameplay/Obstacle.cs
+++ LostInSpace/Assets/Scripts/Scripts_Gameplay/Obstacle.cs
@@ -8,9 +8,10 @@ public class Obstacle : MonoBehaviour
     void Start()
     {
         float randomSize = Random.Range(0.5f, 2.0f);
+        float randomForce = Random.Range(25f, 200f);
         transform.localScale = new Vector3(randomSize, randomSize, 1);
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.right * 100);
+        rb.AddForce(Vector2.right * randomForce);
     }
 
     void Update()
