4e893cf1405c14c0000252ce1bcb178fb0ace304
CallMeDrewChristian <72636893+CallMeDrewChristian@users.noreply.github.com>
2026-01-27 16:20:43 -0500

    Applied Random Size

--- LostInSpace/Assets/Scripts/Scripts_Gameplay/Obstacle.cs
+++ LostInSpace/Assets/Scripts/Scripts_Gameplay/Obstacle.cs
@@ -4,8 +4,8 @@ public class Obstacle : MonoBehaviour
 {
     void Start()
     {
-        transform.localScale = new Vector3(2, 2, 1);
-
+        float randomSize = Random.Range(0.5f, 2.0f);
+        transform.localScale = new Vector3(randomSize, randomSize, 1);
     }
 
     void Update()
