6e5874ca96503c44a0b9bf58121bdab0f4f407dd
CallMeDrewChristian <72636893+CallMeDrewChristian@users.noreply.github.com>
2026-01-27 16:19:47 -0500

    Added Obstacle Script & Size Adjustment

---
+++ LostInSpace/Assets/Scripts/Scripts_Gameplay/Obstacle.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    void Start()
+    {
+        transform.localScale = new Vector3(2, 2, 1);
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
