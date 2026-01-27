c2639ba30f5ccd3ae43e5436116185d1234f4ae1
CallMeDrewChristian <72636893+CallMeDrewChristian@users.noreply.github.com>
2026-01-27 16:21:47 -0500

    Added RigidBody Force

--- LostInSpace/Assets/Scripts/Scripts_Gameplay/Obstacle.cs
+++ LostInSpace/Assets/Scripts/Scripts_Gameplay/Obstacle.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+
+    Rigidbody2D rb; 
+
     void Start()
     {
         float randomSize = Random.Range(0.5f, 2.0f);
         transform.localScale = new Vector3(randomSize, randomSize, 1);
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.right * 100);
     }
 
     void Update()
